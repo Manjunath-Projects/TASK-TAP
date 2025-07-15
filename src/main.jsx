5ae4b973928f837ce508575eb8a2d5bc615e270f
MANJUNATH PATIL <patilmanju4321@gmail.com>
2025-07-15 12:32:26 +0530

    Router Updation

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
+// ReactDOM v18+ rendering
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/TASK-TAP">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
